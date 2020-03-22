c2bb7c7b9795d36440cd04558554cbfef4a063e1
rahamathid <rahamathid@yahoo.com>
2020-03-22 18:09:05 +0800

    new

--- Azure-CreateVM.ps1
+++ Azure-CreateVM.ps1
@@ -4,22 +4,27 @@ $password = ConvertTo-SecureString “Mysore11!” -AsPlainText -Force
 
 $Cred = New-Object System.Management.Automation.PSCredential (“rahamath@outlook.com”, $password) 
 
+Write-Host "Connecting to Azure portal now"
 Connect-AzAccount -Subscription 'a0cdbf8e-57ce-4f3c-a387-5a10b681ba11' -Tenant "05c575ab-b007-47e7-a0ea-f8f2564e6097" -Credential $Cred
 
-#New-AzResourceGroup -Name $ResouceGroup -Location "East Asia"
+If (!(Get-AzResourceGroup -Name $ResouceGroup)) {
+    New-AzResourceGroup -Name $ResouceGroup -Location "East Asia"
+}
 
 $password = ConvertTo-SecureString “Passw0rd1234” -AsPlainText -Force
 $Cred1 = New-Object System.Management.Automation.PSCredential (“vagrantuser1”, $password)
 
+Write-Host "Creating new VM"
 New-AzVm -ResourceGroupName $ResouceGroup -Name $VMBuild -Image "MyWindowsOSImage" `
     -Location "East Asia" `
     -VirtualNetworkName "myVnet" `
     -SubnetName "mySubnet" `
     -SecurityGroupName "myNetworkSecurityGroup" `
-    -PublicIpAddressName "myPublicIpAddress" `
+    -PublicIpAddressName "myPublicIP" `
     -OpenPorts 80,3389 `
-    -Credential $Cred1
+    -Credential $Cred1 | Out-Null
 
+Write-Host "VM creation completed; reading public IP now"
 $vm = Get-AzVM -Name $VMBuild -ResourceGroupName $ResouceGroup
 $nicname = $vm.NetworkProfile[0].NetworkInterfaces.id.Split('/')[-1]
 $nic = Get-AzNetworkInterface -name $nicname -ResourceGroupName $vm.ResourceGroupName
@@ -30,21 +35,24 @@ $PubObj = Get-AzPublicIpAddress -ResourceGroupName $vm.ResourceGroupName | Where
 
 $PublicIP = $PubObj.IpAddress
 
-$PublicIP
+Write-Host "Public IP is: $PublicIP"
 
 Read-Host "check URL"
 
+$WebReq = ""
 $WebReq = Invoke-WebRequest -Uri $PublicIP/default.html
 If ($($WebReq.Content) -like "*Rahamath*") {
     Write-Host "Server build completed sucessfully"
     Read-Host "check URL 2"
-    Remove-AzVM -name $VMbuild -ResourceGroupName $Vm.ResourceGroupName -Force
+    Write-Host "Deleting VM"
+    Remove-AzVM -name $VMbuild -ResourceGroupName $Vm.ResourceGroupName -Force | Out-Null
 
+    Write-host "Disassociate NIC card"
     $nic = Get-AzNetworkInterface -Name $VMBuild -ResourceGroup $ResouceGroup
     $nic.IpConfigurations.publicipaddress.id = $null
     Set-AzNetworkInterface -NetworkInterface $nic | Out-Null
 
-    Remove-AzPublicIpAddress -Name myPublicIpAddress -ResourceGroupName $ResouceGroup -Force
+    #Remove-AzPublicIpAddress -Name myPublicIpAddress -ResourceGroupName $ResouceGroup -Force
 }
 Else {
     Write-Host "Server build failed"
